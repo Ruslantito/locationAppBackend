a5874fd9d0951fe089e7016546bb50989936da15
Rustam Zavkibekov <r.zavkibekov@gmail.com>
2020-10-09 21:43:23 +0500

    step_3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 // create application
 const express = require("express");
 app = express();
+const port = process.env.PORT || 5000;
 
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
-app.listen(5000, () => console.log("Application are listening on port 5000"));
+app.listen(port, () => console.log(`Application are listening on port ${port}!`));
 
 module.exports = app;
